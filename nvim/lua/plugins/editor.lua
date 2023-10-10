8e94b8df909a78b9322f17ab715661b962657b1c
Mayant Mukul <mayantmukul@outlook.in>
2023-10-09 18:31:10 -0700

    change color for indent blanklines

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -16,11 +16,19 @@ return {
     {
         'lukas-reineke/indent-blankline.nvim',
         main = "ibl",
-        opts = {
-            scope = {
-                show_start = false
+        config = function()
+            local highlight = {
+                "MutedIndent"
             }
-        }
+            local hooks = require("ibl.hooks")
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "MutedIndent", { fg = "#23212f" })
+            end)
+            require("ibl").setup {
+                indent = { highlight = highlight },
+                scope = { show_start = false }
+            }
+        end
     },
     {
         'folke/zen-mode.nvim',
