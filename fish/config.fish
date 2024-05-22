4d9fc1ee7ae28091e133918a0e96284d0e1489a8
mayant15 <mayantmukul@outlook.in>
2024-05-22 08:05:52 -0700

    fish

---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+  abbr -a gco git checkout
+  abbr -a ga git add
+  abbr -a gl "git log --oneline"
+  abbr -a gcm "git commit -m"
+  abbr -a gco "git checkout"
+  abbr -a gs "git status"
+  abbr -a lg "lazygit"
+
+  abbr -a to "~/code/dotfiles/tmux-sessionizer"
+  abbr -a tm "tmux"
+  abbr -a tl "tmux ls"
+  abbr -a ta "tmux attach"
+
+  abbr -a jo "j open"
+  abbr -a jt "j tasks"
+
+  fish_config theme choose "rose-pine"
+
+  pyenv init - | source
+end
